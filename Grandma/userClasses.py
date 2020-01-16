6551e4bb51d277be813f9010bda60e1dda1f95b7
Andre Nordlund <andre1.nordlund@gmail.com>
2020-01-16 13:57:29 +0100

    Add user class

---
+++ Grandma/userClasses.py
@@ -0,0 +1,8 @@
+class Grandson:
+    """A class for user using the bot"""
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.timer = 0
+        self.love = 0
+        self.requests = 0
