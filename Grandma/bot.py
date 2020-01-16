2dd016c84ef14bbf01002729f2268bc945758d3e
Andre Nordlund <andre1.nordlund@gmail.com>
2020-01-16 14:09:47 +0100

    Use file for token

--- Grandma/bot.py
+++ Grandma/bot.py
@@ -8,6 +8,8 @@ import userClasses
 
 bot = commands.Bot(command_prefix='.')
 
+grandsons = []
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -23,9 +25,7 @@ async def urban(ctx, arg1):
         embed = getWebsite.getUrban(('https://www.urbandictionary.com/define.php?term={}').format(arg1))
         await ctx.send(embed=embed)
     except:
-        e = sys.exc_info()[0]
-        embed = "Not enough love from my grandchildren to perform request :( or becuse of "+e
-        await ctx.send(embed)
+        await ctx.send(await ctx.send("Not enough love from my grandchildren to perform request :frowning:"))
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -42,8 +42,20 @@ async def roll(ctx, dice: str):
 @bot.command()
 async def reminder(ctx, arg1, arg2):
     """Sets a reminder for a given amount of time"""
-    await ctx.send(result)
+    if isinstance(arg2, int):
+        embed = discord.Embed()
+        embed.title = "Reminder set"
+        embed.add_field(name="Reminder", value=arg1)
+        embed.add_field(name="Set for", value=arg2)
+        embed.set_footer(text=("Requested by "+ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+        grandsons.append(userClasses.Grandson(ctx.message.author.name, ctx.message.author.id))
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Invalid value for time")
 
+@bot.command()
+async def users(ctx):
+    await ctx.send(((d.name, d.id) for d in grandsons))
 @bot.command() #Change img and gif to use urls instead of downloading
 async def img(ctx, arg1):
     """Searches for an image"""
@@ -78,4 +90,6 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send('{0.name} joined, grab a cookie!'.format(member))
 
-bot.run('token')
+file = open('token', 'r')
+token = file.readline().rstrip()
+bot.run(token)
